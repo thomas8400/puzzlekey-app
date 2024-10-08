2ae254cc9651c153458a1b4a482cd6fbfc45c389
thomas8400 <thomas178419@gmail.com>
2024-10-08 23:19:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
       title: {
         fr: "La demeure des Tic-Tac",
         en: "The Tic-Tac Residence",
-        es: "La morada de los Tic-Tac",
+        es: "La casa di Tic-Tac",
         it: "La dimora dei Tic-Tac"
       },
       correctAnswers: ["8-10-2-20","8 - 10 - 2 - 20"],
